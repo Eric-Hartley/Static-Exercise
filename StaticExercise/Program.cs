fd573020ff56a943f088bd836177e0de1f06b3c0
Eric Hartley <spiffydude0@gmail.com>
2024-09-21 17:51:18 -0500

    Completed Exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            double tempInF = 100;
+
+            Console.WriteLine(
+                "Degrees C: " +
+                System.Math.Round(TempConverter.FahrenheitToCelsius(tempInF), 2)
+                );
+
+            double tempInC = 37.78;
+            Console.WriteLine(
+                "Degrees F: " +
+                System.Math.Round(TempConverter.CelsiusToFahrenheit(tempInC), 2)
+                );
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempInF)
+        {
+            // (32°F − 32) × 5/9 = 0°C
+            return (tempInF - 32) * 5 / 9;
+        }
+        public static double CelsiusToFahrenheit(double tempInC)
+        {
+            //(0°C × 9/5) + 32 = 32°F
+
+            return (tempInC * 9 / 5) + 32;
+        }
+    }
+}
